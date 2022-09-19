fd6b1b0bf839c784a54c639f57a75df8219dfece
PULICCE MATEO <46956808@ort>
2022-09-19 11:51:39 -0300

    avance

---
+++ Assets/Scripts/CarroNetScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarroNetScript : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject computerPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InstantiateComputer()
+    {
+        Instantiate(computerPrefab, spawnPoint.position, Quaternion.identity);
+    }
+}
